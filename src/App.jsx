f11dd8b7a727f3a102ca2d2c5444fa37ecab3749
AbiAnton <gale112358@gmail.com>
2025-12-31 01:39:21 -0600

    Navbar and filter button

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Profile({name, shared_classes, similar_interests}){
+  return(
+    <div>
+      <h3>{name}</h3>
+      <p>Shared Classes: {shared_classes}</p>
+      <p>Similar Interests: {similar_interests}</p>
+    </div>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [savedProfiles, setSavedProfiles] = useState(0)
 
   return (
-    <>
+    <main>
+      <navbar>
+        <div aria-label='Home'>Home</div>
+        <div aria-label='About'>About</div>
+        <div aria-label='Saved Profiles'>Saved Profiles: {savedProfiles}</div>
+        <div aria-label='Settings'>Settings</div>
+      </navbar>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button aria-label='Filter Profiles'>Filter</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
